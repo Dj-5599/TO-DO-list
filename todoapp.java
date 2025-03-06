5ecd69c8b3ff29d3ceac62c1303701bb9dcc8e82
Dj-5599 <don.johny@uk.ey.com>
2025-03-06 13:15:25 +0000

    todostartup - welcome message

---
+++ todoapp.java
@@ -0,0 +1,11 @@
+
+
+public class todoapp {
+    public static void main(String[] args) {
+        System.out.println("welcome to to-do list");
+        System.out.println("a simple java spring app");
+        System.out.println("application in on process");
+    }
+
+    
+}
